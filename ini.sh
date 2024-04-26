4f8b4916d48e045d2799ca1816f4ac9e1be2e163
Shusei_Akiya <81072466+Cola0912@users.noreply.github.com>
2024-04-26 10:45:29 +0900

    Update ini.sh

--- ini.sh
+++ ini.sh
@@ -6,6 +6,7 @@ sudo apt upgrade -y
 echo "Hello World"
 echo "export PS1='\[\e[01;32m\]\u@\h\[\e[00m\] \e[1;35m\t \[\e[01;34m\]\w\[\e[00m\]\n\[\e[1;36m\]$\[\e[37m\]'" >> ~/.bashrc
 echo "alias lsdev='echo "by-id is" && ls //dev/serial/by-id/* && echo -e "\nby-path is" && ls //dev/serial/by-path/*'" >> ~/.bashrc
+echo "alias updg='echo sudo apt update && sudo apt upgrade -y'" >> ~/.bashrc
 source ./.bashrc
 
 ##よく使う物のインストール
